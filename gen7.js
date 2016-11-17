0d3dea6faf6bb8943252a343485a4be60f37109c
Alistair McKinlay <alistair@10people.co.uk>
2016-11-17 20:39:00 +0000

    Seperate into gen 6 and gen 7, and add _pkFile

---
+++ gen7.js
@@ -0,0 +1,6 @@
+'use strict';
+// Reference: https://projectpokemon.org/wiki/Pokemon_X/Y_3DS_Structure
+
+exports.parseBuffer = (buf, {parseNames = false, gen = 6, language}) => {
+  throw new Error('Gen 7 not implemented yet');
+};
