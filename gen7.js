b3e00a968dbd3d4d06ce9789f6983b78ed02871d
Alistair McKinlay <alistair@10people.co.uk>
2016-11-18 06:33:47 +0000

    Add hyper training.

--- gen7.js
+++ gen7.js
@@ -4,6 +4,14 @@ const common = require('./common');
 exports.parseBuffer = (buf, {parseNames = false, language} = {}) => {
   const data = common.parseBuffer(buf, {parseNames, language});
 
+  const hyperTraining = buf.readUInt16LE(0xDE);
+  data.hyperTrainedHP = Boolean(hyperTraining & 0x01);
+  data.hyperTrainedAtk = Boolean(hyperTraining & 0x02);
+  data.hyperTrainedDef = Boolean(hyperTraining & 0x04);
+  data.hyperTrainedSpAtk = Boolean(hyperTraining & 0x08);
+  data.hyperTrainedSpDef = Boolean(hyperTraining & 0x10);
+  data.hyperTrainedSpe = Boolean(hyperTraining & 0x20);
+
   if (parseNames) {
     common.assignReadableNames(data, language);
   }
